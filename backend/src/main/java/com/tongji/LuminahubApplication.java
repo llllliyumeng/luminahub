877f141422f2270f6e2e86f3c589a1b60e1a5113
llllliyumeng <2082282412@qq.com>
2025-12-03 10:00:00 +0800

    feat(core): wire backend bootstrap and shared config

---
+++ backend/src/main/java/com/tongji/LuminahubApplication.java
@@ -0,0 +1,12 @@
+package com.tongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuminahubApplication.class, args);
+    }
+}
